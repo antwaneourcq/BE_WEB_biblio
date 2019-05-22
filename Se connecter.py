c0481468f5d3cd337e0ef39d4bc400079b649d0a
loulidanyl <45450321+loulidanyl@users.noreply.github.com>
2019-05-22 10:02:02 +0200

    Add files via upload
    update

--- Se connecter.py
+++ Se connecter.py
@@ -8,15 +8,26 @@ from google.auth.transport.requests import Request
 
 import SGBD
 
-""""gerer les ID des events + gerer le fait de devoir se co à chaque fois + faire un if pour ne pas ajouter un event 
-s'il y en a deja un de prevu à cette heure + ne pas pouvoir reserver hors des horaires d'ouverture """
+# Alcove Europe - Toulouse - 1 - Grise
+id_objet_reservable = 1
 
+def get_id_utilisateur(mail):
+    SGBD.createConnection()
+
+    SGBD.closeConnection()
+    return id_utilisateur
+
+id_utilisateur = get_id_utilisateur()
+
+""""gerer les ID des events + gerer le fait de devoir se co à chaque fois + faire un if pour ne pas ajouter un event 
+s'il y en a deja un de prevu à cette heure + ne pas pouvoir reserver hors des horaires d'ouverture + creer une fonction duree
++ faire une fonction qui trouve l'id de l'utilisateur en cours"""
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-def se_connecter():
 
+def se_connecter():
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -39,11 +50,9 @@ def se_connecter():
     service = build('calendar', 'v3', credentials=creds)
 
 
-
-def creer_event(date,heure_debut,heure_fin,promo):
-    #format 'AAAA-MM-JJ' date
-    #format 'HH-MM-SS' heure_d
-    heure_fin = 0
+def creer_event(date, heure_debut, heure_fin, promo,nb_pers):
+    # format 'AAAA-MM-JJ' date
+    # format 'HH-MM-SS' heure_d
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -68,13 +77,13 @@ def creer_event(date,heure_debut,heure_fin,promo):
     event = {
         'summary': 'Occupé',
         'location': '',
-        'description': promo,
+        'description': promo + str(nb_pers),
         'start': {
-            'dateTime': date+'T'+heure_debut+'+02:00',
+            'dateTime': date + 'T' + heure_debut + '+02:00',
             'timeZone': 'Europe/Paris',
         },
         'end': {
-            'dateTime': date+'T'+heure_fin+'+02:00',
+            'dateTime': date + 'T' + heure_fin + '+02:00',
             'timeZone': 'Europe/Paris',
 
         },
@@ -82,11 +91,15 @@ def creer_event(date,heure_debut,heure_fin,promo):
 
     event = service.events().insert(calendarId='primary', body=event).execute()
     print('Event created: %s' % (event.get('htmlLink')))
+    cnx = SGBD.createConnection()
+    SGBD.ajout_reserver(cnx, 2, date, 120, heure_debut, id_objet_reservable, id_utilisateur, nb_pers)
+    SGBD.closeConnection()
 
-def supprimer_event(date,heure_debut,promo):
-    #format 'AAAA-MM-JJ' date
-    #format 'HH-MM-SS' heure_d
-    heure_fin =  heure_debut
+
+def supprimer_event(date, heure_debut, promo):
+    # format 'AAAA-MM-JJ' date
+    # format 'HH-MM-SS' heure_d
+    heure_fin = heure_debut
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -113,11 +126,11 @@ def supprimer_event(date,heure_debut,promo):
         'location': '',
         'description': promo,
         'start': {
-            'dateTime': date+'T'+heure_debut+'+02:00',
+            'dateTime': date + 'T' + heure_debut + '+02:00',
             'timeZone': 'Europe/Paris',
         },
         'end': {
-            'dateTime': date+'T'+heure_fin+'+02:00',
+            'dateTime': date + 'T' + heure_fin + '+02:00',
             'timeZone': 'Europe/Paris',
 
         },
@@ -149,18 +162,19 @@ def prochains_event():
     service = build('calendar', 'v3', credentials=creds)
 
     # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
     print('Getting the upcoming 10 events')
     events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
-                                        orderBy='startTime').execute()
+                                          maxResults=10, singleEvents=True,
+                                          orderBy='startTime').execute()
     events = events_result.get('items', [])
 
     if not events:
         print('No upcoming events found.')
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'],event.get('id'))
+        print(start, event['summary'], event.get('id'))
+
 
 if __name__ == '__main__':
     # supprimer_event(0,0)
